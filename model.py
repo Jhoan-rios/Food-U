b8af2238abcdd642248e7382416a3016a89b7731
David Rios <yosoydrios@gmail.com>
2026-03-24 21:41:49 -0500

    cree la clase Pedido con sus atributos

--- model.py
+++ model.py
@@ -1,13 +1,40 @@
 from mimetypes import init
+class  Producto:
+    def __init__(self,nombre:str,precio:float,tiempo_preparacion:int,disponible:bool):
+        self.nombre:str =nombre
+        self.precio:float = precio
+        self.tiempo_preparacion:int = tiempo_preparacion
+        self.disponibile:bool= disponible
+
+
+
+
+
+
+
+class Pedido:
+    def __init__(self, id: int, usuario: "Usuario", estado: str, tiempo_estimado: int, total: float):
+        self.id: int = id
+        self.usuario: Usuario = usuario
+        self.productos: list = []
+        self.estado: str = estado
+        self.tiempo_estimado: int = tiempo_estimado
+        self.total: float = total
+
 
 
 class Usuario:
-    def __init__(self,id:int,nombre:str,correo:str,tiempo_disponible:int):
-        self.id:int=id
-        self.nombre:str=nombre
-        self.correo:str=correo
-        self.historial_pedidos:list [Pedido]= []
-        self.tiempo_disponible:int=tiempo_disponible
+    def __init__(self, id: int, nombre: str, correo: str, tiempo_disponible: int):
+        self.id: int = id
+        self.nombre: str = nombre
+        self.correo: str = correo
+        self.historial_pedidos: list[Pedido] = []
+
+    def realizarPedido(self,produtos:list[Producto])->Pedido:
+        pass
+
+
+
 
 
 
@@ -24,14 +51,7 @@ class Vendedor:
 
 
 
-class  Producto:
-    def __init__(self,int:int,nombre:str,precio:float,tiempo_preparacion:int,disponible:bool):
 
-        self.int:int = int
-        self.nombre:str =nombre
-        self.precio:float = precio
-        self.tiempo_preparacion:int = tiempo_preparacion
-        self.disponibile:bool=disponible
 
 
 
