cf9f48a423d8a1cdb17b0a3bc3960fbe4029af8d
David Rios <yosoydrios@gmail.com>
2026-03-26 14:14:00 -0500

    cree clase interfaz con sus atributos

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -1,88 +1,19 @@
-
 from modelo.model import Usuario
-from modelo.model import SistemaFoodU
 from modelo.model import Vendedor
+from modelo.model import Producto
+from modelo.model import SistemaFoodU
 
 
 class Interfaz:
-    sistema =SistemaFoodU()
-
-    def datos_usuarios(self):
-        print("Ingrese los siguientes datos")
-
-        id= input("ID:")
-        nombre=input("Nombre:")
-        correo = input("Correo:")
-
-        usuario = Usuario(id,nombre,correo)
-
-
-
-        SistemaFoodU.registrar_usuario(usuario)
-
-
-    def datos_vendedor(self):
-        print("Ingrese los siguientes datos")
-        nombre = input("Nombre:")
-        vendedor= Vendedor(nombre)
-        SistemaFoodU.registrar_vendedor(vendedor)
-
-
-    def mostrar_vendedores(self):
-        print()
-        for i in self.sistema.vendedor:
-            print(i)
+    class Interfaz:
+        def __init__(self):
+            self.sistema = SistemaFoodU()
+            self.id_usuario = 1
+            self.id_producto = 1
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    def show_menu(self):
-        print("BIENVENIDO A EL SISTEMA FOOTU-UNIVERSIDAD DE MEDELLIN\n")
-
-        opcion = input("SELECCIONE EL TIPO DE USUARIO QUE ERES: U) USUARIO V) VENDEDOR")
-
-        if opcion == "U":
-
-            while True:
-                print("1)Registrar Usuario")
-                print("2)Crear Pedido")
-
-                opcion = int(input("ingrese alguna opcion:"))
-
-                if opcion == 1:
-                    self.datos_usuarios()
-
-
-
-
-
-
-
-        elif opcion == "V":
-
-            while True:
-                print("1)Resgistrar Vendedor")
-                print("2)Gistion De Pedidos")
-
-                opcion = int(input("ingrese alguna opcion:"))
-
-                if opcion == 1:
-                    self.datos_vendedor()
-
-                if opcion == 2:
-
-                    self.mostrar_vendedores()
 
 
 
