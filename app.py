dfa74aae261486ce3024d44548be60cbd6eeea75
David Rios <yosoydrios@gmail.com>
2026-05-16 19:42:37 -0500

    conectar Flask con modelo
    conecte clask con modelo y persistencia del sistema

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
 import re
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from modelo.model import Usuario, Vendedor, Producto, SistemaFoodU
+from modelo.persistencia import guardar_datos, cargar_datos
 
 app = Flask(__name__)
 app.secret_key = "foodu-udem-2024"
+
+
+#ESTADO GLOBAL DEL SISTEMA
+
+
+sistema=SistemaFoodU()
+id_usuario, id_producto = cargar_datos()
+
+
 
