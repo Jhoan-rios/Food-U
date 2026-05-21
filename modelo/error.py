018eb783d6f40e6a042279faf8147f5d51cb1092
Andres_Rosas <Andres.erc2022@gmail.com>
2026-05-21 13:12:52 -0500

    added some exception classes

--- modelo/error.py
+++ modelo/error.py
@@ -1,2 +1,29 @@
-class Exceptions:
+class FoodUException(Exception):
     pass
+
+class UsuarioException(FoodUException):
+    pass
+
+
+class UsuarioDuplicadoError(UsuarioException):
+    pass
+
+
+class CorreoRegistradoError(UsuarioException):
+    pass
+
+
+class UsuarioNoEncontradoError(UsuarioException):
+    pass
+
+class VendedorException(FoodUException):
+    pass
+
+
+class VendedorDuplicadoError(VendedorException):
+    pass
+
+
+class VendedorNoEncontradoError(VendedorException):
+    pass
+
