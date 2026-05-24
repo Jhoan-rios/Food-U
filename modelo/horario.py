67bba9585f90b641908149803d781efafd1fd74e
Andres_Rosas <Andres.erc2022@gmail.com>
2026-05-23 23:38:38 -0500

    created document horario.py added import

---
+++ modelo/horario.py
@@ -0,0 +1,25 @@
+from datetime import datetime, time, timedelta
+
+
+DIAS = ["lunes", "martes", "miércoles", "jueves", "viernes"]
+
+
+EDIFICIOS = {
+    "bloque 1": "restaurante coliseo",
+    "bloque 2": "restaurante coliseo",
+    "bloque 3": "restaurante ingenieria",
+    "bloque 4": "restaurante ingenieria",
+    "bloque 5": "restaurante ingenieria",
+    "bloque 6": "restaurante ingenieria",
+    "bloque 7": "restaurante ingenieria",
+    "bloque 10": "kiosko comunicaciones",
+    "bloque 11": "kiosko comunicaciones",
+    "bloque 12": "kiosko comunicaciones",
+    "bloque 14": "restaurante derecho",
+    "bloque 15": "restaurante derecho",
+    "bloque 16": "restaurante derecho",
+    "bloque 17": "cafeteria teatro",
+    "bloque 18": "cafeteria teatro",
+    "biblioteca": "cafetería central",
+    "default": "cafetería central",
+}
